381dd6ed0a9ba95d9d59f10c7fb1d107c7b74392
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-11-14 12:14:36 +0100

    actions: implemented driftdetection

--- deployments/main.tf
+++ deployments/main.tf
@@ -19,6 +19,8 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
+
+  tags = local.tags
 }
 
 resource "azurerm_subnet" "main" {
